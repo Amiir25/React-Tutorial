3b3d0e6ae35b01b5e4ebaae24577be79e5c9eb9e
Amiir25 <amirsadik636@gmail.com>
2026-03-08 15:17:59 +0300

    Update App.jsx

--- 04-managing-state/projects/src/App.jsx
+++ 04-managing-state/projects/src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import SignUpPage from '../01-signup-form/SignUpPage'
-import UserDirectory from '../02-lve-user-directory/UserDirectory'
+import UserDirectory from '../02-live-user-directory/UserDirectory'
+import CartManager from '../03-shopping-cart-manager/CartManager'
 
 const App = () => {
   return (
     <div>
       {/* <SignUpPage/> */}
-      <UserDirectory/>
+      {/* <UserDirectory/> */}
+      <CartManager/>
     </div>
   )
 }
