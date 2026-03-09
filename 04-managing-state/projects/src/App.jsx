749a493cc17d71ab67a23c44dac1572a9eec84bc
Amiir25 <amirsadik636@gmail.com>
2026-03-09 22:14:19 +0300

    Update App.jsx

--- 04-managing-state/projects/src/App.jsx
+++ 04-managing-state/projects/src/App.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import SignUpPage from '../01-signup-form/SignUpPage'
 import UserDirectory from '../02-live-user-directory/UserDirectory'
 import CartManager from '../03-shopping-cart-manager/CartManager'
+import ThemeSwitcher from '../04-theme-switcher-dashboard/ThemeSwitcher'
 
 const App = () => {
   return (
     <div>
       {/* <SignUpPage/> */}
       {/* <UserDirectory/> */}
-      <CartManager/>
+      {/* <CartManager/> */}
+      <ThemeSwitcher/>
     </div>
   )
 }
