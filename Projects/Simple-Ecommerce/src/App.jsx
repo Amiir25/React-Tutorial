c3edc788f64685d5b25f22e3e28bd82d42d0f30f
Amiir25 <amirsadik636@gmail.com>
2026-03-12 11:23:50 +0300

    Add context provider in App.jsx

--- Projects/Simple-Ecommerce/src/App.jsx
+++ Projects/Simple-Ecommerce/src/App.jsx
@@ -3,17 +3,20 @@ import Home from './pages/Home'
 import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthContext'
 
 const App = () => {
   return (
-    <div className='px-50 bg-gray-200'>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/auth' element={<Auth/>} />
-        <Route path='/checkout' element={<Checkout/>} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className='px-10 md:px-20 lg:px-50 bg-gray-200 h-screen'>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/auth' element={<Auth/>} />
+          <Route path='/checkout' element={<Checkout/>} />
+        </Routes>
+      </div>
+    </AuthProvider>
   )
 }
 
