3b9f516ed75f5cfc94f18243fa173de577fee705
Amiir25 <amirsadik636@gmail.com>
2026-03-13 14:49:24 +0300

    Rename file names

--- 04-managing-state/projects/src/App.jsx
+++ 04-managing-state/projects/src/App.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import SignUpPage from '../01-signup-form/SignUpPage'
 import UserDirectory from '../02-live-user-directory/UserDirectory'
-import CartManager from '../03-shopping-cart-manager/CartManager'
-import ThemeSwitcher from '../04-theme-switcher-dashboard/ThemeSwitcher'
+import CartManager from '../05-shopping-cart-manager/CartManager'
+import ThemeSwitcher from '../06-theme-switcher-dashboard/ThemeSwitcher'
+
 
 const App = () => {
   return (
     <div>
-      {/* <SignUpPage/> */}
-      {/* <UserDirectory/> */}
-      {/* <CartManager/> */}
+      {/* <SignUpPage/>
+      <UserDirectory/>
+      <CartManager/> */}
       <ThemeSwitcher/>
     </div>
   )
