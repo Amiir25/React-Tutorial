ae3200e91babd68f2c59604f5df07082cc6058d1
Amiir25 <amirsadik636@gmail.com>
2026-03-13 10:05:57 +0300

    Add CartContext

--- Projects/Simple-Ecommerce/src/App.jsx
+++ Projects/Simple-Ecommerce/src/App.jsx
@@ -5,19 +5,22 @@ import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
 import AuthProvider from './context/AuthContext'
 import ProductDetails from './pages/ProductDetails'
+import CartProvider from './context/CartContext'
 
 const App = () => {
   return (
     <AuthProvider>
-      <div className='px-10 md:px-20 lg:px-50 bg-gray-200 min-h-screen'>
-        <Navbar/>
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='/auth' element={<Auth/>} />
-          <Route path='/checkout' element={<Checkout/>} />
-          <Route path='/products/:productId' element={<ProductDetails/>} />
-        </Routes>
-      </div>
+      <CartProvider>
+        <div className='px-10 md:px-20 lg:px-50 bg-gray-200 min-h-screen'>
+          <Navbar/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/auth' element={<Auth/>} />
+            <Route path='/checkout' element={<Checkout/>} />
+            <Route path='/products/:productId' element={<ProductDetails/>} />
+          </Routes>
+        </div>
+      </CartProvider>
     </AuthProvider>
   )
 }
