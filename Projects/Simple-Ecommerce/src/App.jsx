93954561e796e52c3d58a09b0d1ea7cea5fed3d3
Amiir25 <amirsadik636@gmail.com>
2026-03-13 07:37:53 +0300

    Add ProductDetails component and style update

--- Projects/Simple-Ecommerce/src/App.jsx
+++ Projects/Simple-Ecommerce/src/App.jsx
@@ -4,16 +4,18 @@ import Auth from './pages/Auth'
 import Checkout from './pages/Checkout'
 import Navbar from './components/Navbar'
 import AuthProvider from './context/AuthContext'
+import ProductDetails from './pages/ProductDetails'
 
 const App = () => {
   return (
     <AuthProvider>
-      <div className='px-10 md:px-20 lg:px-50 bg-gray-200 h-screen'>
+      <div className='px-10 md:px-20 lg:px-50 bg-gray-200 min-h-screen'>
         <Navbar/>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/auth' element={<Auth/>} />
           <Route path='/checkout' element={<Checkout/>} />
+          <Route path='/products/:productId' element={<ProductDetails/>} />
         </Routes>
       </div>
     </AuthProvider>
