22697567474966d90afcf9f23d1e1d3e04cef06a
Amiir25 <amirsadik636@gmail.com>
2026-03-13 10:09:01 +0300

    Cart and checkout features

--- Projects/Simple-Ecommerce/src/pages/ProductDetails.jsx
+++ Projects/Simple-Ecommerce/src/pages/ProductDetails.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom'
 import { getProductById } from '../data/products';
+import { useCart } from '../context/CartContext';
 
 const ProductDetails = () => {
 
     const { productId } = useParams();
     const [product, setProduct] = useState();
     const navigate = useNavigate();
+    const { cartItems, addToCart } = useCart();
 
     useEffect(() => {
         const foundProduct = getProductById(productId);
@@ -24,6 +26,10 @@ const ProductDetails = () => {
         return <h1>Loading...</h1>
     }
 
+    const prodcutInCart = cartItems.find(item => item.id === product.id);
+    const productQuantityLabel = prodcutInCart ? `(${prodcutInCart.qty})` : "";
+
+
     return (
         <section className='flex items-start gap-6 mt-10 bg-white p-4 rounded'>
             <div className='h-100 flex-1'>
@@ -35,8 +41,11 @@ const ProductDetails = () => {
                 <h1 className='text-2xl font-semibold'>{product.name}</h1>
                 <p className='text-blue-600 font-bold my-4'>${product.price}</p>
                 <p>{product.description}</p>
-                <button className='mt-4 bg-blue-600 text-white px-4 py-1 rounded'>
-                    Add to Cart</button>
+                <button
+                onClick={() => addToCart(product.id)}
+                className='mt-4 bg-blue-600 text-white px-4 py-1 rounded cursor-pointer'>
+                    Add to Cart { productQuantityLabel }
+                </button>
             </div>
         </section>
     )
